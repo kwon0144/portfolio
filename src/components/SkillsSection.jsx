5c286d531728fb4aba8474b9c4e096fed94f4fdb
kwon0144 <kinsey.w5505@gmail.com>
2025-08-20 04:01:34 +1000

    adjust skills section ui

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -12,25 +12,29 @@ const SkillsSection = () => {
     return (
         <section id="skills" className="py-24 px-4 relative bg-secondary/30">
         <div className="container mx-auto max-w-5xl">
-            <h2 className="text-2xl font-semibold text-left">
-            My <span className="text-primary">Skills</span>
-            </h2>
+            <div className="grid-cols-1 md:grid-cols-2 flex flex-col md:flex-row gap-12 justify-between mb-6">
+                {/* Header */}
+                <h2 className="text-2xl font-semibold text-left">
+                    My <span className="text-primary">Skills</span>
+                </h2>
 
-            <div className="flex flex-wrap justify-center gap-4 mb-12">
-                {categories.map((category, index) => (
-                    <button
-                        key={index}
-                        onClick={() => setActiveCategory(category)}
-                        className={cn(
-                            "px-5 py-2 rounded-full transition-colors duration-300 capitalize",
-                            activeCategory === category 
-                            ? "bg-primary text-primary-foreground" 
-                            : "bg-foreground/10 text-foreground hover:bg-primary/50"
-                        )}
-                    >
-                        {category}
-                    </button>
-                ))}
+                {/* Category Buttons */}
+                <div className="flex flex-wrap justify-end gap-4">
+                    {categories.map((category, index) => (
+                        <button
+                            key={index}
+                            onClick={() => setActiveCategory(category)}
+                            className={cn(
+                                "text-sm px-5 rounded-full transition-colors duration-300 capitalize",
+                                activeCategory === category 
+                                ? "bg-primary text-primary-foreground" 
+                                : "bg-foreground/10 text-foreground hover:bg-primary/50"
+                            )}
+                        >
+                            {category}
+                        </button>
+                    ))}
+                </div>
             </div>
 
             <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6">
