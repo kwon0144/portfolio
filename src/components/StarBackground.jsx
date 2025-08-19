017c860dc0e74c1e5e08dbd60bfe565d69e31071
kwon0144 <kinsey.w5505@gmail.com>
2025-08-20 00:09:20 +1000

    adjust meteor size

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -74,7 +74,7 @@ const StarBackground = () => {
                 className='meteor animate-meteor' 
                 style={{
                     width: `${meteor.size * 50}px`,
-                    height: `${meteor.size * 2}px`,
+                    height: `${meteor.size * 0.5}px`,
                     left: `${meteor.x}%`,
                     top: `${meteor.y}%`,
                     delay: meteor.opacity,
