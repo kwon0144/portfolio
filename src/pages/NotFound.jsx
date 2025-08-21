768f1883d28a98b44136961b1cab7622ee8625d4
kwon0144 <kinsey.w5505@gmail.com>
2025-08-21 20:33:41 +1000

    ui: update page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import StarBackground from '../components/StarBackground'
+import Navbar from '../components/Navbar'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+
+      {/* Background Effects */}
+      <StarBackground />
+
+      {/* Navbar */}
+      <Navbar />
+      <div className='py-24 px-4 relative'>
+        Page Not Found
+      </div>
+    </div>
   )
 }
 
